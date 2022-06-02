2256652cc093fd557c309dc839c21c3c8b5fb0a1
ellieshami <eli.shami@mail.huij.ac.il>
2022-06-02 22:03:19 +0300

    deleted empty row

--- task1/next_event/pre_process.py
+++ task1/next_event/pre_process.py
@@ -3,7 +3,6 @@ from ..common import UNUSED_COLUMNS, parse_time
 def preprocess(data):
     parse_time(data)
     data = data[data['linqmap_city'] == 'תל אביב - יפו']
-    
     data = data.drop(columns=UNUSED_COLUMNS)
     data.dropna(inplace=True)
     return data
