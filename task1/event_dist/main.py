7a3651fbca1aa0e9a52b1b691f49b84c7bed5dd8
jonatan bar <jonybh99@gmail.com>
2022-06-02 17:47:16 +0300

    event_dist preprocess basic

--- task1/event_dist/main.py
+++ task1/event_dist/main.py
@@ -1,21 +1,23 @@
 from task1.common import load_data
+from task1.event_dist.pre_process import preprocess
 # from .utils import get_arguments
 
 
 def main():
     # args = get_arguments()
     data = load_data()
-
+    fit(data)
 
 def fit(data):
     df = preprocess(data)
-    model = fit(df)
-    save_model(model)
+    print(df.head(200))
+    # model = fit(df)
+    # save_model(model)
 
 def predict(data):
     df = preprocess(data)
-    model = load_model()
-    pred = predict(model, df)
+    # model = load_model()
+    # pred = predict(model, df)
 
 
 
--- task1/event_dist/pre_process.py
+++ task1/event_dist/pre_process.py
@@ -1,44 +1,32 @@
-import pandas as pd
-def preprocess(data, question):
-    return clean_data(data, question)
+from ..common import UNUSED_COLUMNS, parse_time
+def preprocess(data):
+    parse_time(data)
 
-def clean_data(data, question):
-    """
-
-    Parameters
-    ----------
-    question: "Q1" or "Q2" question we want to clean the data for
-    Returns: clean data frame
-    -------
-
-    """
-    # convert date to datetime format and add time features
-    data['update_date'] = pd.to_datetime(data['update_date'], unit='ms')
-    data['day_of_week'] = data['update_date'].dt.day_name()
-    data['hour'] = data['update_date'].dt.hour
-
-    # clean data uniquely for Question 1
-    if question == 'Q1':
-        data = data[data['linqmap_city'] == 'תל אביב - יפו']
-
-    # clean data uniuqely for Question 2
-    if question == 'Q2':
-        columns_to_drop_Q2 = ['linqmap_subtype', 'linqmap_magvar', 'x', 'y']
-        data = data.drop(columns=columns_to_drop_Q2)
-
-    # remove columns we don't want
-    columns_to_drop_Q1 = ['OBJECTID', 'pubDate', 'linqmap_reportDescription',
-                          'linqmap_reportDescription', 'linqmap_street', 'linqmap_nearby',
-                          'linqmap_reportMood', 'linqmap_roadType', 'linqmap_reportMood',
-                          'linqmap_reportRating', 'linqmap_expectedBeginDate', 'linqmap_expectedEndDate',
-                          'linqmap_reliability', 'nComments', 'update_date', 'linqmap_city']
-    data = data.drop(columns=columns_to_drop_Q1)
-
+    columns_to_drop_Q2 = ['linqmap_subtype', 'linqmap_magvar', 'x', 'y']
+    data = data.drop(columns=columns_to_drop_Q2)
+    data = data.drop(columns=UNUSED_COLUMNS)
     return data
 
-def structure_data():
-    pass
 
-def add_features():
-    pass
+# def clean_data(data):
+#     """
+
+#     Parameters
+#     ----------
+#     Returns: clean data frame
+#     -------
+
+#     """
+    # parse_time(data)
+
+
+    # remove columns we don't want
+
+    # return data
+
+# def structure_data():
+#     pass
+
+# def add_features():
+#     pass
 
