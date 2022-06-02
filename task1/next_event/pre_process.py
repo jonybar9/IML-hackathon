a90aadf2eaad7fd2998fdf187314d5689375cf14
Chaim Gross <chaim.gross>
2022-06-03 00:10:06 +0300

    fixed split_train_data_to_X_and_y

--- task1/next_event/pre_process.py
+++ task1/next_event/pre_process.py
@@ -5,6 +5,7 @@ import pandas as pd
 GROUP_COL_NAME = 'test_set'
 BULK_SIZE = 4
 
+
 def preprocess(data):
     parse_time(data)
     data = data[data['linqmap_city'] == 'תל אביב - יפו']
@@ -12,17 +13,17 @@ def preprocess(data):
     data = data.drop(columns=UNUSED_COLUMNS)
     data.dropna(inplace=True)
 
-    data = pd.concat([data, pd.get_dummies(data['linqmap_type'], drop_first=True)], axis=1)
-    data = pd.concat([data, pd.get_dummies(data['linqmap_subtype'], drop_first=True)], axis=1)
     data = pd.concat([data, pd.get_dummies(data['hour'], drop_first=True)], axis=1)
     data = data.drop(columns=["linqmap_type", "linqmap_subtype", "day_of_week", 'hour'])
 
     return data
 
+
 def group_by_bulk(data_with_groups):
     bulks_list = [x for _, x in data_with_groups.groupby(data_with_groups[GROUP_COL_NAME])]
     bulks = np.array(bulks_list).shape
     return bulks
+
 
 def bulk_bootsraping(data):
     col_names = list(data.columns)
@@ -45,17 +46,26 @@ def _add_bulk_number(df, i):
 
     return df
 
+
+def make_dummies(data):
+    data = pd.concat([data, pd.get_dummies(data['linqmap_type'], drop_first=True)], axis=1)
+    data = pd.concat([data, pd.get_dummies(data['linqmap_subtype'], drop_first=True)], axis=1)
+    data = data.drop(columns=["linqmap_type", "linqmap_subtype"])
+    return data
+
 def split_train_data_to_X_and_y(lst):
     """
     lst: list of dataframes with five rows
     returns: X: numpy matrix where each row is four samples flattened
              y: pandas DataFrame where each row is the labels we need to predict of corresponding row in X
     """
-    X = np.array([df[:4].to_numpy().flatten() for df in lst])
     y = [df[4:] for df in lst]
     y = pd.concat(y)
     y = y[['linqmap_type', 'linqmap_subtype', 'x', 'y']]  # keep only labels we need to predict
+
+    X = np.array([make_dummies(df)[:4].to_numpy().flatten() for df in lst])
     return X, y
+
 
 def merge_test_data(lst):
     """
@@ -65,7 +75,6 @@ def merge_test_data(lst):
     """
     X = np.array([df[:4].to_numpy().flatten() for df in lst])
     return X
-
 
 # def group_by_time(data):
 
