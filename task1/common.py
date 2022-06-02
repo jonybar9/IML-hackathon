855ea5bed8014a3838fb2205193a80f0f8027a3b
jonatan bar <jonybh99@gmail.com>
2022-06-02 19:24:54 +0300

    extract day_of_week & hour from pubDate

--- task1/common.py
+++ task1/common.py
@@ -2,9 +2,9 @@ import pandas as pd
 
 def parse_time(data):
     # convert date to datetime format and add time features
-    data['update_date'] = pd.to_datetime(data['update_date'], unit='ms')
-    data['day_of_week'] = data['update_date'].dt.day_name()
-    data['hour'] = data['update_date'].dt.hour
+    data['pubDate'] = pd.to_datetime(data['pubDate'])
+    data['day_of_week'] = data['pubDate'].dt.day_name()
+    data['hour'] = data['pubDate'].dt.hour
 
 
 UNUSED_COLUMNS = ['OBJECTID', 'pubDate', 'linqmap_reportDescription',
