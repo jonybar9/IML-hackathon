f01ede08ee64ab0f9e7f4f43bbe4e1c61d69334b
LihiShalmon <94780547+LihiShalmon@users.noreply.github.com>
2022-06-03 01:29:48 +0300

    Q2 - average creation and csv maker

--- task1/event_dist/pre_process.py
+++ task1/event_dist/pre_process.py
@@ -1,9 +1,9 @@
-from ..common import UNUSED_COLUMNS, parse_time
+from ..common import UNUSED_COLUMNS, parse_time ,parse_time_publish
 import  numpy as np
 
 def preprocess(data):
     parse_time(data)
-
+    parse_time_publish(data)
     columns_to_drop_Q2 = ['linqmap_subtype', 'linqmap_magvar', 'x', 'y']
     data = data.drop(columns=columns_to_drop_Q2)
     data = data.drop(columns=UNUSED_COLUMNS)
@@ -13,12 +13,18 @@ def preprocess(data):
 
 def time_section(data):
     data["section"] = np.zeros(data.shape[0])
-    data.section[(data['hour'] >= 8) & (data['hour'] <= 10)] = 1
-    data.section[(data['hour'] >= 12) & (data['hour'] <= 14)] = 2
-    data.section[(data['hour'] >= 16) & (data['hour'] <= 18)] = 3
+    data.section[(data['pub_hour'] >= 8) & (data['pub_hour'] <= 10)] = 1
+    data.section[(data['pub_hour'] >= 12) & (data['pub_hour'] <= 14)] = 2
+    data.section[(data['pub_hour'] >= 16) & (data['pub_hour'] <= 18)] = 3
 
     # used to seperate to all time section
     # print(data["section"].value_counts())
+    return data
+
+def convert_weekday(data):
+    days = {'Sunday': 1, 'Monday': 2, 'Tuesday': 3, 'Wednesday': 4, 'Thursday': 5, 'Friday': 6, 'Saturday': 7}
+    for day in days:
+        data.pub_day[data.pub_day == day] = days[day]
     return data
 
 # def clean_data(data):
