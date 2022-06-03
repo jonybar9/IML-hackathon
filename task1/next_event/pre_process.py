75e5015ea2b954654df832c770c07da2f0101ee0
ellieshami <eli.shami@mail.huij.ac.il>
2022-06-03 04:10:43 +0300

    bulk np.series

--- task1/next_event/pre_process.py
+++ task1/next_event/pre_process.py
@@ -19,7 +19,7 @@ preprocess(pd.read_csv(r"..\datasets\waze_data.csv"))
 
 def group_by_bulk(data_with_groups):
     bulks_list = [x for _, x in data_with_groups.groupby(data_with_groups[GROUP_COL_NAME])]
-    bulks = np.array(bulks_list)
+    bulks = pd.Series(np.array(bulks_list))
     return bulks
 
 
